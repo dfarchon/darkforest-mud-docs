57717a851a8c4eb1d39f0f05f3493cb67dcf903c
Scissors <Scissors@funblocks.xyz>
2024-10-25 19:03:50 +0800

    保存当前更改

---
+++ pages/enter/_meta.js
@@ -0,0 +1,10 @@
+export default {
+
+    21: "Opening the Game",
+    22: "Log in Accounts",
+    23: "Welcome pane & Tutorial pane",
+    24: "Settings & Private Key Export",
+    25: "Making your first move!",
+    26: "Exploring the map",
+}
+
